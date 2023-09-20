00b805f3cb3fd2ed0036d5a3ab5e05bd5830fbe2
cami168d <camilla.rasmussen.lu@gmail.com>
2023-09-20 13:18:16 +0000

    New edit

--- test_packages.R
+++ test_packages.R
@@ -1,8 +1,3 @@
 library(DBI)
-<<<<<<< HEAD
-library(tidyverse) # Often used in data science
+library(tidyverse) # Load many packages often used in data science
 library(RPostgres)
-=======
-library(tidyverse) # Load many packages to RStudio
-library(RPostgres)
->>>>>>> 5ce9a2d59cb74f6e88e198847cf0838c7312fc8d
